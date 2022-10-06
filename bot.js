755cd43b9bd27b85168934c0ce7fc4759b61278a
MirHeyder03 <mhyahyayev03@gmail.com>
2022-10-06 21:05:25 +0400

    minor change

--- bot.js
+++ bot.js
@@ -2,21 +2,18 @@ import TelegramBot from "node-telegram-bot-api";
 import fetch from "node-fetch";
 import * as dotenv from "dotenv";
 dotenv.config();
-
-// .env
+//! .env
 const API_KEY = process.env.API_KEY;
 const token = process.env.BOT_TOKEN;
-
 const bot = new TelegramBot(token, { polling: true });
-
 /* Weather Api */
-const url = `https://api.openweathermap.org/data/2.5/weather?q=Baku&appid=${API_KEY}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`;
 const getData = async () => {
   const response = await fetch(url);
   const data = await response.json();
   var selsi = Math.floor(data.main.temp - 273).toString();
-  bot.on("text", (msg) => {
-    chatId = msg.chat.id;
+  bot.on("message", (msg) => {
+    let chatId = msg.chat.id;
     bot.sendMessage(chatId, `Temperatur  ${selsi}°C`);
   });
 };
